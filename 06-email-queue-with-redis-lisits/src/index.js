9b73bfae07a3aa0cc2e418e3eaf1bc737968744e
Parth <parth.manocha2901@gmail.com>
2026-05-23 17:33:21 +0530

    So carrying the learning forward, implmented profile-cache, email-queue using redis with hands on practice.

---
+++ 06-email-queue-with-redis-lisits/src/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import Redis from "ioredis";
+
+const app = express();
+
+app.use(express.json());
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+const QUEUE_KEY = "queue:emails";
+
+app.post("/emails", async (req, res) => {
+  const job = {
+    to: req.body.to,
+    subject: req.body.subject || "No subject",
+    body: req.body.body || "No content",
+    createdAt: new Date().toISOString(),
+  };
+
+  await redis.lpush(QUEUE_KEY, JSON.stringify(job));
+  res.json({ queued: true, job });
+});
+
+app.get("/gEmails/process-one", async (req, res) => {
+  const rawJob = await redis.rpop(QUEUE_KEY);
+
+  if (!rawJob) {
+    return res.json({ message: "No jobs in the queue" });
+  }
+
+  const job = JSON.parse(rawJob);
+  // Simulate email sending
+
+  res.json({ message: "Email send", job });
+});
+
+
+app.listen(3000,()=>{
+    console.log("Server running at PORT 3000");
+    
+});
